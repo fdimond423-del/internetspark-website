cbc3b2f4cf21d0edde616eba6b1e741fb07a185d
Fenil Limbachiya <fenil@example.com>
2026-05-29 17:15:36 +0530

    Enhance UI: Enlarge loader logo, upgrade content boxes, add orbital animation

--- js/shared.js
+++ js/shared.js
@@ -99,6 +99,15 @@
   const navPlaceholder = document.getElementById('shared-nav') || document.getElementById('navbar') || document.querySelector('.navbar');
   if (navPlaceholder) navPlaceholder.outerHTML = navHTML;
   else document.body.insertAdjacentHTML('afterbegin', navHTML);
+  // Inject orbital logo animation
+  const orbitalHTML = `
+  <div class="logo-circle">
+    <img src="${base}images/sir.png" style="top: 0; left: 50%; transform: translate(-50%, -50%);" alt="Orbit Logo 1">
+    <img src="${base}images/sir.png" style="bottom: 0; left: 50%; transform: translate(-50%, 50%);" alt="Orbit Logo 2">
+    <img src="${base}images/sir.png" style="top: 50%; left: 0; transform: translate(-50%, -50%);" alt="Orbit Logo 3">
+    <img src="${base}images/sir.png" style="top: 50%; right: 0; transform: translate(50%, -50%);" alt="Orbit Logo 4">
+  </div>`;
+  document.body.insertAdjacentHTML('beforeend', orbitalHTML);
 
   // Inject boxify script
   const boxifyScript = document.createElement('script');
